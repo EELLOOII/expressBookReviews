ebb5cca79e1e0a52455f94451d64fe19f6ad9898
eloi <marcielo.abalos@ionics-ems.com>
2025-06-19 00:52:11 +0800

    FEAT: Update the endpoint for fetching a book based on its title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,7 +35,11 @@ public_users.get("/author/:author", function (req, res) {
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const filteredBooks = Object.values(books).filter(
+    (book) => book.title === title
+  );
+  res.send(filteredBooks);
 });
 
 //  Get book review
